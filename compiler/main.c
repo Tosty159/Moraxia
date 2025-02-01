c11c6d2b43c478e6d35d26474047b6f40dbbc85f
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-02-02 00:31:52 +0100

    Create a lexer instance from Moraxia source code in main.

--- compiler/main.c
+++ compiler/main.c
@@ -2,5 +2,15 @@
 #include "lexer.h"
 
 int main(int argc, char *argv[]) {
-    
+    char path[] = "../src/main.mx";
+
+    FILE *file = fopen(path, "r");
+
+    Lexer *lexer = (Lexer *)malloc(sizeof(Lexer));
+    if (!lexer) {
+        perror("Failed to allocate memory for lexer");
+        exit(1);
+    }
+    lexer->source = file;
+    lexer->position = 0;
 }
