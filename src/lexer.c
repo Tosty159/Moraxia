abf1c65f1ce7646453b5f4d3ad014e634400ecfc
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-02-07 11:27:25 +0100

    Handle number tokens.

--- src/lexer.c
+++ src/lexer.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "lexer.h"
 
 #define INITIAL_BUFFER_SIZE 50
@@ -9,4 +11,72 @@ Lexer *create_lexer(FILE *stream) {
     l->source = stream;
 
     return l;
+}
+
+int handle_number(char **buffer, size_t *buffer_size, FILE *stream, char first) {
+    (*buffer)[0] = first;
+    size_t idx = 0;
+
+    int ch;
+    int is_int = 1;
+    while ((ch = fgetx(stream)) != EOF) {
+        if (!(isdigit(ch) || ch == '.')) {
+            break;
+        }
+
+        if (idx >= *buffer_size - 1) {
+            *buffer_size += 50;
+            char *temp = realloc(*buffer, *buffer_size);
+            if (!temp) {
+                perror("Failed to reallocate memory for buffer");
+                exit(1);
+            }
+            *buffer = temp;
+        }
+        (*buffer)[idx++] = ch;
+
+
+        if (ch == '.') {
+            if (is_int) {
+                is_int = 0;
+            } else {
+                fprintf(stderr, "Unexpected decimal point: %s", *buffer);
+                exit(1);
+            }
+        }
+    }
+
+    return is_int;
+}
+
+Token *next_token(Lexer *lexer) {
+    Token *token = (Token *)malloc(sizeof(Token));
+    if (!token) {
+        perror("Failed to allocate memory for token");
+        exit(1);
+    }
+
+    size_t buffer_size = INITIAL_BUFFER_SIZE;
+    char *value = malloc(buffer_size);
+    if (!value) {
+        perror("Failed to allocate memory for value");
+        exit(1);
+    }
+
+    int ch;
+    while ((ch = fgetc(lexer->source)) != EOF) {
+        if (isspace(ch)) {
+            continue;
+        }
+
+        if (isdigit(ch)) {
+            if (handle_number(&value, &buffer_size, lexer->source, ch)) {
+                token->type = TOKEN_INTEGER;
+            } else {
+                token->type = TOKEN_FLOAT;
+            }
+            token->value = strdup(value);
+            break;
+        }
+    }
 }
