7dd8a6e6518144c68b09689343babbc1408f7a85
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-03-06 21:08:28 +0000

    Make helper functions for lexer.

--- frontend/lib/lexeparser/lexer.ml
+++ frontend/lib/lexeparser/lexer.ml
@@ -1,5 +1,22 @@
+open Token
+
 type lexer = {
   stream: in_channel;
   mutable is_over: bool;
   mutable unget_stack: char list;
 }
+
+let create_lexer stream =
+  {
+    stream = stream;
+    is_over = false;
+    unget_stack = [];
+  }
+
+let read lex =
+  match lex.unget_stack with
+  | [] -> input_char lex.stream
+  | hd :: tl -> lex.unget_stack <- tl; hd
+
+let unget lex ch =
+  lex.unget_stack <- ch :: lex.unget_stack
