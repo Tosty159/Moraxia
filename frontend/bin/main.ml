513da8db91a04b47bfaf075969c34bcc07b11d2e
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-03-09 22:18:17 +0000

    Update main.

--- frontend/bin/main.ml
+++ frontend/bin/main.ml
@@ -1,1 +1,18 @@
-let () = print_endline "Hello, World!"
+open Lexeparser
+
+let () =
+  let args = Sys.argv in
+  let argc = Array.length args in
+  if argc <> 2 then
+    failwith (Printf.sprintf "Usage: %s <moraxia file>" args.(0))
+  else begin
+    let stream = open_in args.(1) in
+    let lex = Lexer.create_lexer stream in
+    let rec print_tokens lex =
+      let t = Lexer.next_token lex in
+      match t with
+      | Token.EOF -> print_endline (Token.show_token t)
+      | _ -> print_endline (Token.show_token t); print_tokens lex
+    in
+    print_tokens lex
+  end
