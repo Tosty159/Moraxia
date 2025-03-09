c30ba5ec35f470d6398c1bf07c49dff6b483dc80
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-03-09 13:50:41 +0000

    Create new tokens for operators.

--- frontend/lib/lexeparser/token.ml
+++ frontend/lib/lexeparser/token.ml
@@ -5,6 +5,13 @@ type token =
   | String of string
   | Bool of bool
   | Operator of string
+  | AssignmentOp of string
+  | TypingOp of string
+  | ArithmeticOp of string
+  | BitLogicOp of string
+  | ComparisonOp of string
+  | CollectiveOp of string
+  | DotOp of string
   | Keyword of string
   | Identifier of string
   | Punctuation of string
@@ -19,6 +26,13 @@ let show_token token =
   | String s -> Printf.sprintf "String(%s)" s
   | Bool b -> Printf.sprintf "Bool(%s)" (string_of_bool b)
   | Operator op -> Printf.sprintf "Operator(%s)" op
+  | AssignmentOp op -> Printf.sprintf "AssignmentOp(%s)" op
+  | TypingOp op -> Printf.sprintf "TypingOp(%s)" op
+  | ArithmeticOp op -> Printf.sprintf "ArithmeticOp(%s)" op
+  | BitLogicOp op -> Printf.sprintf "LogicOp(%s)" op
+  | ComparisonOp op -> Printf.sprintf "ComparisonOp(%s)" op
+  | CollectiveOp op -> Printf.sprintf "CollectiveOp(%s)" op
+  | DotOp op -> Printf.sprintf "DotOp(%s)" op
   | Keyword kw -> Printf.sprintf "Keyword(%s)" kw
   | Identifier id -> Printf.sprintf "Identifier(%s)" id
   | Punctuation punct -> Printf.sprintf "Punctuation(%s)" punct
