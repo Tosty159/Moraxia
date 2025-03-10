6d35f51a83fee5acc1821f95a539f78bef8905e3
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-03-10 01:12:35 +0000

    Rework reading system.

--- frontend/lib/lexeparser/lexer.ml
+++ frontend/lib/lexeparser/lexer.ml
@@ -1,5 +1,7 @@
 open Token
 
+exception ReturnImmediate
+
 type lexer = {
   stream: in_channel;
   mutable is_over: bool;
@@ -15,8 +17,18 @@ let create_lexer stream =
 
 let read lex =
   match lex.unget_stack with
-  | [] -> input_char lex.stream
-  | hd :: tl -> lex.unget_stack <- tl; hd
+  | [] -> (
+      if lex.is_over then
+        None
+      else
+        try
+          Some (input_char lex.stream)
+        with End_of_file -> (
+            lex.is_over <- true;
+            None
+          )
+    )
+  | hd :: tl -> lex.unget_stack <- tl; Some hd
 
 let unget lex ch =
   lex.unget_stack <- ch :: lex.unget_stack
@@ -31,32 +43,45 @@ let is_alnum ch =
   is_alpha ch || is_digit ch
 
 let handle_number lex first =
-  if is_digit first then
-    let rec read_number lex number =
-      match read lex with
-      | ch when is_digit ch -> read_number lex (number ^ (String.make 1 ch))
-      | ch -> unget lex ch; number
-    in
-    let num = read_number lex (String.make 1 first) in
-    Some (Int (int_of_string num))
-  else
-    None
+  try
+    if is_digit first then
+      let rec read_number lex number =
+        let ch = match read lex with
+        | Some ch -> ch
+        | None -> raise ReturnImmediate
+        in
+        match ch with
+        | ch when is_digit ch -> read_number lex (number ^ (String.make 1 ch))
+        | ch -> unget lex ch; number
+      in
+      let num = read_number lex (String.make 1 first) in
+      Some (Int (int_of_string num))
+    else
+      None
+  with ReturnImmediate -> None
 
 let handle_alphabetic lex first =
-  if is_alpha first then
-    let rec read_alpha lex name =
-      match read lex with
-      | ch when is_alnum ch -> read_alpha lex (name ^ (String.make 1 ch))
-      | ch -> unget lex ch; name
-    in
-    let name = read_alpha lex (String.make 1 first) in
-    Some (Identifier name)
-  else
-    None
+  try
+    if is_alpha first then
+      let rec read_alpha lex name =
+        let ch = match read lex with
+        | Some ch -> ch
+        | None -> raise ReturnImmediate
+        in
+        match ch with
+        | ch when is_alnum ch -> read_alpha lex (name ^ (String.make 1 ch))
+        | ch -> unget lex ch; name
+      in
+      let name = read_alpha lex (String.make 1 first) in
+      Some (Identifier name)
+    else
+      None
+  with ReturnImmediate -> None
 
 let read_optional lex expected =
-  let c = read lex in
-  if c = expected then true else (unget lex c; false)
+  match read lex with
+  | Some c -> if c = expected then true else (unget lex c; false)
+  | None -> false
 
 let handle_symbol lex first =
   match first with
@@ -166,26 +191,32 @@ let handle_symbol lex first =
   | _ -> None
 
 let rec handle_whitespace lex =
-  let ch = read lex in
-  match ch with
-  | ' ' | '\t' | '\n' -> handle_whitespace lex
-  | _ -> unget lex ch; ()
+  match read lex with
+  | Some ch -> (
+      match ch with
+        | ' ' | '\t' | '\n' -> handle_whitespace lex
+        | _ -> unget lex ch; ()
+    )
+  | None -> ()
 
 let next_token lex =
   if lex.is_over then
     EOF
   else begin
     handle_whitespace lex;
-    let ch = read lex in
-    match handle_number lex ch with
-    | Some num -> num
-    | None ->
-      match handle_alphabetic lex ch with
-      | Some alpha -> alpha
-      | None ->
-        match handle_symbol lex ch with
-        | Some sym -> sym
+    match read lex with
+    | Some ch -> (
+        match handle_number lex ch with
+        | Some num -> num
         | None ->
-          let message = Printf.sprintf "Invalid character: %c" ch in
-          failwith message
+          match handle_alphabetic lex ch with
+          | Some alpha -> alpha
+          | None ->
+            match handle_symbol lex ch with
+            | Some sym -> sym
+            | None ->
+              let message = Printf.sprintf "Invalid character: %c" ch in
+              failwith message
+      )
+    | None -> EOF
   end
