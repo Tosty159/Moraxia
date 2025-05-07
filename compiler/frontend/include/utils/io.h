321aa18a9ac0c3ae7345b113ceccbac61e76805f
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-05-07 17:37:59 +0100

    Initialize 'io.h'.

---
+++ compiler/frontend/include/utils/io.h
@@ -0,0 +1,13 @@
+#ifndef IO_H
+#define IO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Opens file while making sure it has the specified extension.
+// If the file doesn't exist, it will be created.
+// Returns NULL if the file can't be opened, doesn't exist, or has the wrong extension.
+// If the file is opened successfully, it returns a pointer to the file stream.
+FILE *open_file(const char *filename, const char *mode, const char *extension);
+
+#endif
