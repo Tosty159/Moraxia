bcf7d7e128ae9d6ed4331c7f2aff9e22519e2ed4
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-05-08 18:41:47 +0100

    Handle integers & floats.

--- compiler/frontend/lexeparser/lexer.c
+++ compiler/frontend/lexeparser/lexer.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
+#define INITIAL_BUFFER_SIZE 64
 
 Lexer *lexer_create(FILE *stream) {
     Lexer *lexer = (Lexer *)malloc(sizeof(Lexer));
@@ -113,5 +116,54 @@ Token next_token(Lexer *lexer) {
 
     while (lexer->current_char != '\0') {
         char ch = lexer->current_char;
+
+        if (isdigit(ch)) {
+            size_t size = INITIAL_BUFFER_SIZE;
+            char *result_num = (char *)malloc(size);
+            if (!result_num) {
+                perror("Failed to allocate memory for number");
+                exit(EXIT_FAILURE);
+            }
+
+            result_num[0] = ch;
+            lexer_advance(lexer);
+
+            int is_int = 1; // Assume it's an integer first
+
+            size_t count = 1;
+            while (!lexer->is_over) {
+                if (!(isdigit(lexer->current_char) || lexer->current_char == '.')) {
+                    break;
+                }
+
+                if (count >= size - 1) {
+                    size *= 2;
+                    char *temp = (char *)realloc(result_num, size);
+                    if (!temp) {
+                        perror("Failed to reallocate memory for number");
+                        free(result_num);
+                        exit(EXIT_FAILURE);
+                    }
+                    result_num = temp;
+                    free(temp);
+                }
+                result_num[count++] = lexer->current_char;
+
+                if (lexer->current_char == '.') {
+                    if (!is_int) {
+                        fprintf(stderr, "Error: Multiple decimal points in number: '%s.', line: %lu, pos: %lu\n",
+                            result_num, lexer->line, lexer->column);
+                        free(result_num);
+                        exit(EXIT_FAILURE);
+                    }
+                    is_int = 0; // It's a float
+                }
+
+                lexer_advance(lexer);
+            }
+            result_num[count] = '\0';
+
+            return (Token){TOKEN_INT, result_num, lexer->line, lexer->column};
+        }
     }
 }
