f7efba8238569ec3f190ea5fabb12c7d35a194aa
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-05-08 15:07:13 +0100

    Update main: Open file safely.

--- compiler/frontend/main.c
+++ compiler/frontend/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "utils/io.h"
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    const char *filename = argv[1];
-
-    // TODO: Open file safely
+    FILE *file = open_file(argv[1], "r", ".mx");
 
     return 0;
 }
