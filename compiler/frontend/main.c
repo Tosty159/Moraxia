a60bb4119b8e46cb21e7413a487fd3f198cf3491
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-05-08 23:04:06 +0100

    Update main.

--- compiler/frontend/main.c
+++ compiler/frontend/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 #include "utils/io.h"
+#include "lexeparser/token.h"
+#include "lexeparser/lexer.h"
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -10,6 +12,19 @@ int main(int argc, char *argv[]) {
     }
 
     FILE *file = open_file(argv[1], "r", ".mx");
+
+    Lexer *lexer = lexer_create(file);
+
+    Token token = next_token(lexer);
+    while (token.type != TOKEN_EOF) {
+        char *token_str = token_to_string(token);
+        printf("%s\n", token_str);
+        free(token_str);
+        token = next_token(lexer);
+    }
+    char *token_str = token_to_string(token);
+    printf("%s\n", token_str);
+    free(token_str);
 
     return 0;
 }
