cb335eeb1509f1f9701050acc81161a48eea4414
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-05-08 15:30:53 +0100

    Initialize Lexer.

---
+++ compiler/frontend/include/lexeparser/lexer.h
@@ -0,0 +1,22 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <stdio.h>
+
+#include "lexeparser/token.h"
+
+typedef struct Lexer {
+    FILE *stream;
+    char current_char;
+    unsigned long line;
+    unsigned long column;
+} Lexer;
+
+Lexer *lexer_create(FILE *stream);
+void lexer_destroy(Lexer *lexer);
+
+void lexer_advance(Lexer *lexer);
+
+Token next_token(Lexer *lexer);
+
+#endif
